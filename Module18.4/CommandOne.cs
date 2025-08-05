5062c5d7b497877bb6829a8efcd73906edc9b89d
Serega5555 <lazarpalace@gmail.com>
2025-08-05 11:27:44 +0300

    Create CommandOne.cs

---
+++ Module18.4/CommandOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace Module18._4
+{
+    class CommandOne(Receiver receiver) : Command
+    {
+        Receiver receiver = receiver;
+
+        public override void Run()
+        {
+            Console.WriteLine("Запущен процесс...");
+            Receiver.OperationVideo().GetAwaiter().GetResult(); // Ждем завершения операции
+        }
+    }
+}
