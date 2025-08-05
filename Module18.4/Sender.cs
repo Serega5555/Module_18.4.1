965e3986b4e84f0b7d4359c6beebdb8c40a66278
Serega5555 <lazarpalace@gmail.com>
2025-08-05 11:31:12 +0300

    Create Sender.cs

---
+++ Module18.4/Sender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module18._4
+{
+    class Sender
+    {
+        Command _command;
+
+        public void SetCommand(Command command)
+        {
+            _command = command;
+        }
+        public void Run()
+        {
+            _command.Run();
+        }
+    }
+}
