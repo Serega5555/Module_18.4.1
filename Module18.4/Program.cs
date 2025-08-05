ade9d3309858c1b38572433ebbee517b58ca18b2
Serega5555 <lazarpalace@gmail.com>
2025-08-05 11:28:46 +0300

    Create Program.cs

---
+++ Module18.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Module18._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //создадим отправителя
+            var sender = new Sender();
+
+            //создадим получателя
+            var receiver = new Receiver();
+
+            //создадим команду
+            var commandOne = new CommandOne(receiver);
+
+            //инициализация команды
+            sender.SetCommand(commandOne);
+                        
+            //выполнение
+            sender.Run();
+        }
+    }
+}
